a506480582184cd9a2214d72b783659d41e6fd48
ZachWagner1 <zwag14@yahoo.com>
2020-10-15 20:10:37 -0500

    stripe implementation

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -6,22 +6,37 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/react-hooks';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
 
     console.log(state);
 
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
     useEffect(() => {
         async function getCart() {
-          const cart = await idbPromise('cart', 'get');
-          dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
         };
-      
+
         if (!state.cart.length) {
-          getCart();
+            getCart();
         }
-      }, [state.cart.length, dispatch]);
+    }, [state.cart.length, dispatch]);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
@@ -45,6 +60,20 @@ const Cart = () => {
         );
     }
 
+    function submitCheckout() {
+        const productIds = [];
+
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+
+        getCheckout({
+            variables: { products: productIds }
+        })
+    }
+
     return (
         <div className="cart">
             <div className="close" onClick={toggleCart}>
@@ -57,7 +86,7 @@ const Cart = () => {
                     <div className="flex-row space-between">
                         <strong>Total: ${calculateTotal()}</strong>
                         {Auth.loggedIn() ? (
-                            <button>Checkout</button>
+                            <button onClick={submitCheckout}>Checkout</button>
                         ) : (
                                 <span>(log in to check out)</span>
                             )}
